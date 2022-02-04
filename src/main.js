0868f3cf66f9fd5590282f57965060451a4e8d09
Fairgrove <fagerlundfr@gmail.com>
2022-02-04 20:26:32 +0100

    Buttons are there now, no click interaction yet

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 const width = window.innerWidth;
 const height = window.innerHeight;
+
 const menu = 1
+const nButtons = 3
+const buttonSymbols = ['#', 'Ω', 'N']
+let menuButtons = []
 
 // MAIN
 const canvas = document.querySelector('#canvas');
@@ -9,7 +13,70 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 ctx.filter = 'blur(0px)'
 
-function textLayer(){
+const cursor = {
+    x: width / 2,
+    y: height / 2,
+    mappedX: 0.5,
+    mappedY: 0.5,
+    clickX: 0,
+}
+
+class button{
+    constructor(id, text){
+        this.id = id
+        this.width = 150 //width
+        this.height = 100 //height
+        this.startX = (width/2)-75 //-half width
+        this.startY = 30 //-half height
+
+        this.text = text
+
+        this.offset = 5
+
+        let difference = this.width + this.offset
+        let pos = -((nButtons-1)/2) * difference + ((this.id - 1) * difference)
+
+        this.startX += pos
+    }
+
+    draw(){
+        ctx.beginPath();
+        ctx.rect(
+            this.startX,
+            this.startY,
+            this.width,
+            this.height);
+        ctx.fillStyle = 'rgba(0,100,100,1)'
+        ctx.fill();
+
+        ctx.fillStyle  = 'white';
+        ctx.textAlign = "center"
+        ctx.lineWidth = 1;
+        ctx.font = '50px verdana';
+        ctx.fillText(this.text, this.startX+this.width/2, this.startY+15+this.height/2);
+
+        if (cursor.x >= this.startX && cursor.x <= (this.startX + this.width)){
+            if (cursor.y >= this.startY && cursor.y <= (this.startY + this.height)) {
+                ctx.beginPath();
+                ctx.rect(
+                    this.startX,
+                    this.startY,
+                    this.width,
+                    this.height);
+                ctx.fillStyle = 'rgba(255,255,255,0.5)'
+                ctx.fill();
+            }
+        }
+    }
+
+    click(){
+        if (menu != this.id){
+            menu = this.id
+        }
+    }
+}
+
+function homePage(){
     const box = {
         topLeft: (width/2)-270, //-half width
         bottomLeft: (height/2)-65, //-half height
@@ -41,7 +108,7 @@ function textLayer(){
     ctx.textAlign = "center"
     ctx.lineWidth = 1;
     ctx.font = '30px arial';
-    ctx.fillText('Hi :)', width/2, height/2-21);
+    ctx.fillText('Hi :)' + cursor.clickX, width/2, height/2-21);
 
     ctx.fillStyle  = 'white';
     ctx.textAlign = "center"
@@ -52,25 +119,21 @@ function textLayer(){
 
 }
 
-function menuLayer(){
+function aboutPage(){
 
-    const box = {
-        topLeft: (width/2)-50,
-        bottomLeft: 30,
-        topRight: 100,
-        bottomRight: 70,
-        spacing: 10
+}
+
+function pagePicker(){
+    if (menu == 1) {
+        homePage()
+    } else if (menu == 2) {
+        aboutPage()
     }
+}
 
-    for(var i = -1; i < 1; i++){
-        ctx.beginPath();
-        ctx.rect(
-            box.topLeft,
-            box.bottomLeft,
-            box.topRight,
-            box.bottomRight);
-        ctx.fillStyle = 'rgba(255,255,100,1)'
-        ctx.fill();
+function menuLayer(){
+    for (var i = 0; i < menuButtons.length; i++) {
+        menuButtons[i].draw()
     }
 }
 
@@ -79,16 +142,14 @@ function background(){
     ctx.fillRect(0,0,width,height);
 }
 
-const cursor = {
-    x: width / 2,
-    y: height / 2,
-    mappedX: 0.5,
-    mappedY: 0.5,
+//creating buttons
+for (var i = 1; i < 4; i++) {
+    menuButtons.push(new button(i, buttonSymbols[i-1]))
 }
 
 background()
 menuLayer()
-textLayer()
+homePage()
 
 window.addEventListener("mousemove", (e) => {
     cursor.x = e.clientX;
@@ -96,8 +157,27 @@ window.addEventListener("mousemove", (e) => {
 
     cursor.mappedX = ((cursor.x - width)/width)+1
     cursor.mappedY = ((cursor.y - height)/height)+1
+})
 
+window.addEventListener('mouseup', e => {
+    cursor.clickX = Math.random()
+
+    //cursor.clickX = '0'
+    //cursor.clickX =  = e.offsetX;
+    //y = e.offsetY;
+});
+
+
+function update(){
     background()
     menuLayer()
-    textLayer()
-})
+    pagePicker()
+
+    window.requestAnimationFrame(update)
+}
+
+window.requestAnimationFrame(update)
+
+/*
+    V  ART GOES HERE  V
+*/
